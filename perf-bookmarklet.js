d54b24fee43cf76eca34a87d72699c0527255da1
Thomas Heigl <thomas.heigl@iteratec.de>
2015-03-26 15:19:39 +0100

    Made CSS styles of bookmarklet more generic Made more beautiful Removed link to original performance bar Added separator between scripts

--- perf-bookmarklet.js
+++ perf-bookmarklet.js
@@ -1,6 +1,13 @@
 var body = document.getElementsByTagName("body")[0];
 
+var style = document.createElement("style");
+style.innerText = "#PerfBar > div a { display: inline-block; cursor: pointer; text-decoration: none !important; color: #fff !important; display: inline-block; padding: 5px; }";
+style.innerText += "#PerfBar > div a:hover { background-color: red !important; }";
+style.innerText += "#PerfBar .perfClose { position: absolute; width: 50; text-align: right; top: 0; right: 0; }";
+body.appendChild(style);
+
 var topBarContainer = document.createElement("div");
+topBarContainer.id = "PerfBar";
 topBarContainer.style.color = "#fff";
 topBarContainer.style.position = "absolute";
 topBarContainer.style.top = 0;
@@ -15,10 +22,12 @@ topBar.style.paddingRight = 50;
 topBarContainer.appendChild(topBar);
 
 var scripts = [
+	/*
 	{
 		name:	"Original",
 		href:	"//stevesouders.com/mobileperf/mobileperfbkm.js"
 	},
+	//*/
 	{
 		name:	"Performance Bookmarklet",
 		href:	"https://micmro.github.io/performance-bookmarklet/dist/performanceBookmarklet.min.js"
@@ -33,14 +42,13 @@ var scripts = [
 	},
 	{
 		name:	"DOM Monster",
-		href:	"http://mir.aculo.us/dom-monster/dommonster.js?" + Math.floor((+new Date)/(864e5))
+		href:	"http://mir.aculo.us/dom-monster/dommonster.js"
 	},
 	{
 		name:	"Display Stats",
 		href:	"https://rawgit.com/mrdoob/stats.js/master/build/stats.min.js",
 		onclick:	function(){
 			var displayStatsInterval = setInterval(function(){
-				console.log("hii :)");
 				if(typeof Stats == "function")
 				{
 					clearInterval(displayStatsInterval);
@@ -60,20 +68,14 @@ var scripts = [
 ];
 
 var close = document.createElement("a");
+close.className = "perfClose";
 close.innerText = "close";
-close.style.position = "absolute";
-close.style.width = 50;
-close.style.textAlign = "right";
-close.style.top = 0;
-close.style.right = 0;
-close.style.display = "inline-block";
-close.style.padding = 5;
-close.style.cursor = "pointer";
-close.style.color = "#fff";
 close.onclick = function(){
 	body.removeChild(topBarContainer);
 };
 topBar.appendChild(close);
+
+var maxIndex = (scripts.length - 1);
 
 for(var f in scripts)
 {
@@ -81,15 +83,12 @@ for(var f in scripts)
 	
 	var link = document.createElement("a");
 	link.innerText = script.name;
-		link.href = 'javascript:(function(){';
-			link.href += 'var jselem = document.createElement("script");';
-			link.href += 'jselem.type = "text/javascript";';
-			link.href += 'jselem.src = "' + script.href + '";';
-			link.href += 'body.appendChild(jselem);';
-		link.href += '})()';
-	link.style.display = "inline-block";
-	link.style.padding = 5;
-	link.style.color = "#fff";
+	link.href = 'javascript:(function(){';
+		link.href += 'var jselem = document.createElement("script");';
+		link.href += 'jselem.type = "text/javascript";';
+		link.href += 'jselem.src = "' + script.href + '?' + Math.floor((+new Date)/(864e5)) + '";';
+		link.href += 'body.appendChild(jselem);';
+	link.href += '})()';
 	
 	if(typeof(script.onclick) != null)
 	{
@@ -97,4 +96,11 @@ for(var f in scripts)
 	}
 	
 	topBar.appendChild(link);
+	
+	if(f < maxIndex)
+	{
+		var separatorElem = document.createElement("span");
+		separatorElem.innerHTML = "&middot;";
+		topBar.appendChild(separatorElem);
+	}
 }
