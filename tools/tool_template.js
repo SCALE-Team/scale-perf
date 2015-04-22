cc4dbd2be766aaf1539102bed7f668ff86a181bb
Thomas Heigl <thomas.heigl@iteratec.de>
2015-04-22 09:10:25 +0200

    Added more description to tool template

--- tools/tool_template.js
+++ tools/tool_template.js
@@ -1,22 +1,24 @@
-var DomMonster = function() {
+var ToolTemplate = function() {
+	/* Here in the constructor you initialize your whole class
+	and append your tools elements to the display */
+	
 	return {
-		/* SCALE performance tool IO functions */
-			// The id of the element that contains all tool elements
-			containerId:			"jr_results",
-			
-			// Determines whether the website should be moved down to offer a space for the tool
-			shouldMovePageContent:	true,
-			
-			// will be executed after the tool got loaded
-			onload: function() {},
-			
-			/* This is the destructor, implement it!
-			It should remove:
-			- the container for your tool
-			- further elements you appended to the website
-			- style tags you added to the document
-			- visual styles you appended to elements on the website
-			*/
-			onclose: function() {}
+		// The id of the element that contains all tool elements
+		containerId:			"jr_results",
+		
+		// Determines whether the website should be moved down to offer a space for the tool
+		shouldMovePageContent:	true,
+		
+		// will be executed after the tool got loaded
+		onload: function() {},
+		
+		/* This is the destructor, implement it!
+		It should remove:
+		- the container for your tool
+		- further elements you appended to the website
+		- style tags you added to the document
+		- visual styles you appended to elements on the website
+		*/
+		onclose: function() {}
 	};
 };
